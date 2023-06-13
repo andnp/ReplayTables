9022b455e8f2de6cfe99209c6f9cbbc7482f8edc
github-action-benchmark <github@users.noreply.github.com>
2023-06-13 21:19:33 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for f1f233146432433620581c0aa2d017cd0ca2f4e4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686691172088,
+  "lastUpdate": 1686691173122,
   "repoUrl": "https://github.com/andnp/ReplayTables",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -160,6 +160,86 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000011252221976706263",
             "extra": "mean: 28.417145106394873 usec\nrounds: 13211"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "andnpatterson@gmail.com",
+            "name": "andy",
+            "username": "andnp"
+          },
+          "committer": {
+            "email": "andnpatterson@gmail.com",
+            "name": "andy",
+            "username": "andnp"
+          },
+          "distinct": true,
+          "id": "f1f233146432433620581c0aa2d017cd0ca2f4e4",
+          "message": "ci: run benchmarking ci during pull requests",
+          "timestamp": "2023-06-13T15:18:14-06:00",
+          "tree_id": "1b3e3759990c29d55c0c34fbee7b8c538c27e6f2",
+          "url": "https://github.com/andnp/ReplayTables/commit/f1f233146432433620581c0aa2d017cd0ca2f4e4"
+        },
+        "date": 1686691170551,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_PER.py::TestBenchmarks::test_per_add",
+            "value": 56301.607488830065,
+            "unit": "iter/sec",
+            "range": "stddev: 8.952759193889454e-7",
+            "extra": "mean: 17.76148221342338 usec\nrounds: 6831"
+          },
+          {
+            "name": "tests/test_PER.py::TestBenchmarks::test_per_sample",
+            "value": 4869.18340402361,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000043078324204348335",
+            "extra": "mean: 205.3732457836068 usec\nrounds: 2372"
+          },
+          {
+            "name": "tests/test_PrioritizedHeap.py::TestBenchmarks::test_prioritized_heap_add",
+            "value": 129064.74323708414,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000012410280379800735",
+            "extra": "mean: 7.748049350418343 usec\nrounds: 28328"
+          },
+          {
+            "name": "tests/test_PrioritizedHeap.py::TestBenchmarks::test_prioritized_heap_sample",
+            "value": 2738.401099477378,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00007620932405302823",
+            "extra": "mean: 365.17659892513535 usec\nrounds: 1860"
+          },
+          {
+            "name": "tests/test_ReplayBuffer.py::TestBenchmarks::test_replay_buffer_add",
+            "value": 869628.3885277646,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000062252301957021264",
+            "extra": "mean: 1.1499164622407827 usec\nrounds: 196079"
+          },
+          {
+            "name": "tests/test_ReplayBuffer.py::TestBenchmarks::test_replay_buffer_sample",
+            "value": 11303.015308594491,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000030872842959478823",
+            "extra": "mean: 88.47196723157832 usec\nrounds: 3601"
+          },
+          {
+            "name": "tests/utils/test_SumTree.py::TestBenchmarks::test_sumtree_update",
+            "value": 29128.23678117864,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00000411245478007811",
+            "extra": "mean: 34.33094860881367 usec\nrounds: 20704"
+          },
+          {
+            "name": "tests/utils/test_SumTree.py::TestBenchmarks::test_sumtree_sample",
+            "value": 34514.564145227596,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000016539605989246745",
+            "extra": "mean: 28.9732762028308 usec\nrounds: 12136"
           }
         ]
       },
